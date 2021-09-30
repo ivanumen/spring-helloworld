64d68da677d2d0de65eeb58380b01130f15dc6e8
ivanumen <ivan.umen@mail.ru>
2021-09-30 18:38:05 +0300

    Add work 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean3.getMessage());
+        System.out.println(bean4.getMessage());
+        System.out.println(bean1.equals(bean2));
+        System.out.println(bean3.equals(bean4));
     }
 }
